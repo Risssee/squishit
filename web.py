fd41a0de6af5de9cc64fc27ebd35a7cd896c3f8b
Risssee <nhung6eeeee@gmail.com>
2026-05-11 00:44:47 +0700

    add requirements

--- web.py
+++ web.py
@@ -112,4 +112,4 @@ def compress():
 # CHẠY SERVER
 # ─────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
